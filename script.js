f3696eb1a05a6201a26aa7d7780a0455650ed89b
andzej123 <andzejdemidov@gmail.com>
2023-12-01 23:45:40 +0200

    fixed gaps

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const menu = document.querySelector(".burger_menu");
 const closeButton = document.querySelector(".burger_menu_x");
 
 const myFunction = () => {
-    menu.classList.toggle("burger_menu_hidden");
-}
+  menu.classList.toggle("burger_menu_hidden");
+};
 
 button.addEventListener("click", myFunction);
 closeButton.addEventListener("click", myFunction);
-
-
