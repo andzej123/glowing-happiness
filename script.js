9fcdbdaacfc6bfcde40ae9f5ac17e825d88c52eb
andzej123 <andzejdemidov@gmail.com>
2023-12-03 11:52:43 +0200

    added scroll listener to burger menu

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const button = document.querySelector(".burger_button");
 const menu = document.querySelector(".burger_menu");
 const closeButton = document.querySelector(".burger_menu_x");
 
-const myFunction = () => {
-  menu.classList.toggle("burger_menu_hidden");
-};
-
-// button.addEventListener("click", myFunction);
-// closeButton.addEventListener("click", myFunction);
-
 document.body.addEventListener("click", function (event) {
   if (button.contains(event.target)) {
     menu.classList.toggle("burger_menu_hidden");
@@ -24,4 +17,8 @@ document.body.addEventListener("click", function (event) {
   } else {
     menu.classList.remove("burger_menu_hidden");
   }
+});
+
+addEventListener("scroll", () => {
+  menu.classList.remove("burger_menu_hidden");
 });
