c83845b975ed41793e6b90cc89bb0a7a20253346
andzej123 <andzejdemidov@gmail.com>
2023-12-03 20:21:26 +0200

    fixed upper bg circle and burger circle positions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".burger_button");
 const menu = document.querySelector(".burger_menu");
 const closeButton = document.querySelector(".burger_menu_x");
+const circle = document.querySelector(".circle_top");
+const burgerCircle = document.querySelector(".circle_burger_menu");
 
 document.body.addEventListener("click", function (event) {
   if (button.contains(event.target)) {
@@ -22,3 +24,50 @@ document.body.addEventListener("click", function (event) {
 addEventListener("scroll", () => {
   menu.classList.remove("burger_menu_hidden");
 });
+
+// <><><><><><><><><><> Circles <><><>><><><><><><><><><>><><><><><><><> //
+
+let wi = 780 + (window.innerWidth - 375) * 0.43;
+let circlePosition = (window.innerWidth - wi) / 2;
+let burgerLeft =
+  (window.innerWidth - wi) / 2 - (window.innerWidth / 100) * 20.2;
+let burgerLeftBig = -255 - (window.innerWidth - 625) * 0.70422535;
+
+circle.style.width = `${wi}px`;
+circle.style.height = `${wi}px`;
+circle.style.borderRadius = `${wi}px`;
+circle.style.left = `${circlePosition}px`;
+burgerCircle.style.width = `${wi}px`;
+burgerCircle.style.height = `${wi}px`;
+burgerCircle.style.borderRadius = `${wi}px`;
+
+if (window.innerWidth < 625) {
+  burgerCircle.style.left = `${burgerLeft}px`;
+} else {
+  burgerCircle.style.left = `${burgerLeftBig}px`;
+}
+
+window.addEventListener("resize", myFunction);
+
+function myFunction() {
+  let wi = 780 + (window.innerWidth - 375) * 0.43;
+  let circlePosition = (window.innerWidth - wi) / 2;
+  let burgerLeft =
+    (window.innerWidth - wi) / 2 - (window.innerWidth / 100) * 20.2;
+
+  let burgerLeftBig = -255 - (window.innerWidth - 625) * 0.70422535;
+
+  circle.style.width = `${wi}px`;
+  circle.style.height = `${wi}px`;
+  circle.style.borderRadius = `${wi}px`;
+  circle.style.left = `${circlePosition}px`;
+  burgerCircle.style.width = `${wi}px`;
+  burgerCircle.style.height = `${wi}px`;
+  burgerCircle.style.borderRadius = `${wi}px`;
+
+  if (window.innerWidth < 625) {
+    burgerCircle.style.left = `${burgerLeft}px`;
+  } else {
+    burgerCircle.style.left = `${burgerLeftBig}px`;
+  }
+}
